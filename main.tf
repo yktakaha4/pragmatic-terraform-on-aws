1271de727628f5a13cc5061d89ffa9e0e71d14a6
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-18 14:04:39 +0900

    add apache

--- main.tf
+++ main.tf
@@ -2,6 +2,12 @@ resource "aws_instance" "example" {
   ami = "ami-0f9ae750e8274075b"
   instance_type = "t3.micro"
 
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+
   tags = {
     Name = "example"
   }

373111eb56dd13006664adc90372eecde104020f
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-19 20:32:15 +0900

    add private subnet

--- network.tf
+++ network.tf
@@ -7,3 +7,36 @@ resource "aws_vpc" "example" {
     Name = "example"
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_internet_gateway" "example" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_route" "public" {
+  route_table_id = aws_route_table.public.id
+  gateway_id = aws_internet_gateway.example.id
+  destination_cidr_block = "0.0.0.0/0"
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.example.id
+  cidr_block = "10.0.64.0/24"
+  availability_zone = "ap-northeast-1a"
+  map_public_ip_on_launch = false
+}

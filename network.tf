05fe667b3954c0aa31f55c11e1726cf59cca0e63
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-22 20:44:55 +0900

    use security group module

--- network.tf
+++ network.tf
@@ -112,25 +112,10 @@ resource "aws_route" "private_1" {
   destination_cidr_block = "0.0.0.0/0"
 }
 
-resource "aws_security_group" "example" {
-  name   = "example"
-  vpc_id = aws_vpc.example.id
-}
-
-resource "aws_security_group_rule" "ingress_example" {
-  type              = "ingress"
-  from_port         = "80"
-  to_port           = "80"
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
-}
-
-resource "aws_security_group_rule" "egress_example" {
-  type = "egress"
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
+module "example_sg" {
+  source      = "./security_group"
+  name        = "module-sg"
+  vpc_id      = aws_vpc.example.id
+  port        = 80
   cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.example.id
 }

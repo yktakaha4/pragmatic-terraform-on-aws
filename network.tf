832d1045305c9cf0e774ebdf9e3fbd699e534354
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-06-22 20:33:12 +0900

    add firewall

--- network.tf
+++ network.tf
@@ -111,3 +111,26 @@ resource "aws_route" "private_1" {
   nat_gateway_id         = aws_nat_gateway.example.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+resource "aws_security_group" "example" {
+  name   = "example"
+  vpc_id = aws_vpc.example.id
+}
+
+resource "aws_security_group_rule" "ingress_example" {
+  type              = "ingress"
+  from_port         = "80"
+  to_port           = "80"
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
+
+resource "aws_security_group_rule" "egress_example" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.example.id
+}
